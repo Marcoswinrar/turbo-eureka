74eedf390ab38746636ef45434fb1f9f22790ac6
Marcos Eduardo Winiarski <marcos.winiarski@ambevtech.com.br>
2022-05-04 21:05:50 -0300

    Criação de componente para estudo de useCallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ListExpand from './components/list-expand';
+import Callback from './hooks/useCallBack';
 
 function App() {
   return (
     <div className="App">
       <h2>React API</h2>
       <ListExpand />
+      <Callback/>
     </div>
   )
 }
---
+++ src/hooks/useCallBack/index.js
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react"
+
+const Callback = () => {
+    
+  const [usuario, setUsuario] = useState({nome: 'Marcos', categoria: 'Dev'})
+  const [categoria, setCategoria] = useState('')
+
+
+  // Callback guarda como referencia o valor dos itens alterados, cuidar com o 2 param
+  const atualizaUsuario = useCallback(() => {
+    setUsuario({nome: 'Eduardo', categoria: categoria})
+  }, [])
+
+
+  console.log("NOME USUÁRIO:", usuario.nome)
+  console.log("CATEGORIA:", usuario.categoria)
+
+
+  // 1 Log: Nome: Marcos, Categoria: Dev
+  // 2 Click categoria - 2 Log: Nome: Eduardo, Categoria: " "
+  return (
+    <div>
+      <button onClick={atualizaUsuario}>Atualizar nome do usuário</button>
+      <button onClick={() => setCategoria("QA")}>Atualizar categoria</button>
+    </div>
+  )
+}
+
+export default Callback
